e6d03b0fe367220ebc7f82d78ca46170a3a3378c
Ajith S <143083090+ajiaravi19@users.noreply.github.com>
2023-11-22 10:25:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [[$GIT_BRANCH == "origin/dev"]]; then 
+if [[ $GIT_BRANCH == "origin/dev" ]]; then 
 	./build.sh
         docker login -u ajithselvam -p dckr_pat_zpnY-4BY5Z0suXXM1vWHOi-vNsQ
        
         docker tag myreactimg ajithselvam/dev
         docker push ajithselvam/dev
 
-elif [[$GIT_BRANCH ==  "origin/main"]]; then
+elif [[ $GIT_BRANCH ==  "origin/main" ]]; then
 	./build.sh
         docker login -u ajithselvam -p dckr_pat_zpnY-4BY5Z0suXXM1vWHOi-vNsQ
       
